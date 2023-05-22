3fcf73eb5d13835370a7b41161641d8e9769886f
vzavolsky <vzavolsky@ya.ru>
2023-05-22 19:36:57 +0300

    Course 03 - HW 03 - SQL Initialized commit.

---
+++ src/main/java/com/zavolsky/course_03/Hw06ListsApplication.java
@@ -0,0 +1,13 @@
+package com.zavolsky.course_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw06ListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw06ListsApplication.class, args);
+	}
+
+}
